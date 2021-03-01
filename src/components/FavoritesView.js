d306dbb7ad027f3728816d032702b801c08b3eca
aligendza <73717510+aligendza@users.noreply.github.com>
2021-03-01 17:04:59 -0500

    favorites renders to page

--- src/components/FavoritesView.js
+++ src/components/FavoritesView.js
@@ -1,31 +1,43 @@
-import react, { useEffect,useState } from 'react';
+import react, { useEffect, useState } from 'react';
 // import Navbar from '../components/Navbar';
 import './FavoritesView.css';
 
 const FavoritesView = (props) => {
-    const[favorites,setFavorites] =useState([]);
+    const [favorites, setFavorites] = useState([]);
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
         fetch('http://localhost:3000/recipe', {
             method: 'GET',
-            headers: new Headers ({
+            headers: new Headers({
                 'Content-Type': 'application/json',
                 'Authorization': props.token
             })
-        }).then ((res) => res.json())
-        .then((logData) => {
-            setFavorites(logData)
-        })
+        }).then((res) => res.json())
+            .then((logData) => {
+                setFavorites(logData)
+                console.log(logData)
+            })
     }
 
-    useEffect(()=>{
-        handleSubmit();
-    }, [])
+    // useEffect(()=>{
+    //     handleSubmit();
+    // }, [])
 
 
     return (
         <div>
-            <h1 className="favorites">View My Favorites</h1>
+            <h1 className="favorites">Favorites</h1>
+            <button onClick={handleSubmit}>View Favorites</button>
+            {favorites.map(favorite => {
+                return (
+                    <div>
+                        <p></p>
+                        <p>{favorite.title}</p>
+                        <p>{favorite.rId}</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
