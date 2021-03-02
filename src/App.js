5730aac358944a62dc1160995089fccf9e0d9715
Jeremy Guler <bubulldog20@yahoo.com>
2021-03-01 19:36:40 -0500

    updateform in progress

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Sitebar from './components/Navbar';
 import './App.css';
 import SearchView from './components/SearchView';
 import FavoritesView from './components/FavoritesView';
-
+import UpdateForm from './components/UpdateForm';
 function App() {
   const pages = [
     {component: <SearchView />, title:"Search"},
@@ -14,24 +14,20 @@ function App() {
   //const [currentView, setCurrentView] = useState('Favorites')
   const [currentView, setCurrentView] = useState('Search')
   const [component, setComponent] = useState(pages[0].component);
-
   useEffect( () => {
     if (localStorage.getItem('token')) {
       setSessionToken(localStorage.getItem('token'));
     }
   }, []);
-
   const updateToken = (newToken) => {
     localStorage.setItem('token', newToken);
     setSessionToken(newToken);
     console.log(sessionToken); 
   };
-
   const clearToken = () => {
     localStorage.clear();
     setSessionToken('');
   }
-
   const protectedViews = () => {
     // under what circumstances would this be false? udateToken both sets the same token in the state variable and the
     // local storage. works just as well to check if a token exists without comparing the state to the local storage
@@ -42,14 +38,13 @@ function App() {
       : <Auth updateToken={updateToken} />
       )
   }
-
   return (
     <div className="App">
       <Sitebar clearToken={clearToken} setComponent={setComponent}/>
       {protectedViews()}
       <FavoritesView />
+      <UpdateForm />
     </div>
   );
 }
-
 export default App;
--- src/components/UpdateForm.js
+++ src/components/UpdateForm.js
@@ -1,6 +1,31 @@
 import {useState} from 'react'
-
-
-
-
+const UpdateForm = ({userRecipe}) => {
+    const [title, setTitle] = useState(userRecipe.title)
+    const [recipeURL, setRecipeURL] = useState(userRecipe.url)
+    const updateRecipe = () => {
+        console.log(userRecipe.id, title, recipeURL )
+        fetch(`http://localhost:3000/update/${userRecipe.id}`, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: title,
+                recipeURL: recipeURL
+            })
+        })
+    }
+    const deleteRecipe = (id) => {
+        fetch(`http://localhost:3000/delete/${id}`, {
+            method: "DELETE"
+        })
+    }
+    return (
+        <div>
+            <h3>Title: <input placeholder={userRecipe.title} onChange={e => setTitle(e.target.value)}/></h3>
+            <h4>Url: <input placeholder={userRecipe.recipeURL} onChange={e => setRecipeURL(e.target.value)}/></h4>
+            <button onClick={() => updateRecipe()}>Update Recipe Entry</button>
+        </div>
+    )
+}
 export default UpdateForm
