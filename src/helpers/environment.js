0c17bb5dbce2b8863a68d903ef3509abb08120e7
Abby King <abbyking@Abbys-MacBook-Pro.local>
2021-03-06 14:40:52 -0500

    backslashes

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -3,10 +3,10 @@ let APIURL = ''
 switch (window.location.hostname) {
     case 'localhost':
     case '127.0.0.1':
-        APIURL = 'http://localhost:3000'
+        APIURL = 'http://localhost:3000/'
         break
     case 'jg-yum.herokuapp.com':
-        APIURL = 'https://ak-yum.herokuapp.com'
+        APIURL = 'https://ak-yum.herokuapp.com/'
 }
 
 export default APIURL;
