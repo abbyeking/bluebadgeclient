1f69789b7fe4cd1e2142414394c93566d2aaaa90
Abby King <abbyking@Abbys-MacBook-Pro.local>
2021-03-08 20:27:08 -0500

    hustin help

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -1,74 +1,125 @@
-import React, { useState } from 'react';
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
-import styled from 'styled-components';
-import StyledButton from '../components/Styles/Button'
-import APIURL from '../helpers/environment'
+// import React, { useState } from 'react';
+// import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+// import styled from 'styled-components';
+// import StyledButton from '../components/Styles/Button'
+// import APIURL from '../helpers/environment'
+
+// const Signup = (props) => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [noPass, setNoPass] = useState(false);
+//   const [wrongEmail, setWrongEmail] = useState(false);
+
+
+//   let handleSubmit = (event) => {
+//     event.preventDefault();
+//     let R = new RegExp(/[^@]+@[^@]+\.[^@]+/g);
+//     if (!R.test(email)) {
+//       setWrongEmail(true)
+//       console.log("hello", email, wrongEmail)
+//     }
+//     if (wrongEmail) {
+//       if (password.length > 4 && password.length < 14) {
+//         setNoPass(false);
+//         console.log(email, password);
+//         fetch(`${APIURL}/user/signup`,
+//           {
+//             method: 'POST',
+//             body: JSON.stringify({ user: { email: email, password: password } }),
+//             headers: new Headers({ 'Content-Type': 'application/json' })
+//           }
+//         )
+//           .then(
+//             (response) => {
+//               console.log(response);
+//               return response.json()
+//             })
+//           .then(
+//             (data) => {
+//               console.log(data);
+//               props.updateToken(data.sessionToken)
+//             }
+//           )
+//       } else {
+//         setNoPass(true);
+//       }
+//     } else {
+//       (console.log("please correct email format"))
+//     }
+//   }
+
+
+
+//   return (
+//     <div>
+//       <h1>Signup</h1>
+//       <Form onSubmit={handleSubmit}>
+//         <FormGroup>
+//           <Label htmlFor='email'>Email</Label>
+//           <Input required onChange={(e) => setEmail(e.target.value)} email='email' value={email} />
+//           <p style={{ color: "red" }} > {(wrongEmail) ? "Email is invalid" : ""}</p>
+//         </FormGroup>
+//         <FormGroup>
+//           <Label htmlFor='password'>Password</Label>
+//           <Input required onChange={(e) => setPassword(e.target.value)} name='password' value={password} />
+//           <p style={{ color: "red" }} > {(noPass) ? "Password must be between 4 and 13 characters" : ""}</p>
+//         </FormGroup>
+//         <StyledButton type='submit'>Signup</StyledButton>
+//       </Form>
+//     </div>
+//   );
+// };
+
+// export default Signup;
+
+import React, { useState } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import styled from "styled-components";
+import StyledButton from "../components/Styles/Button";
+import APIURL from "../helpers/environment";
 
 const Signup = (props) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [noPass, setNoPass] = useState(false);
-  const [wrongEmail, setWrongEmail] = useState(false);
-
-
-  let handleSubmit = (event) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
     event.preventDefault();
-    let R = new RegExp(/[^@]+@[^@]+\.[^@]+/g);
-    if (!R.test(email)) {
-      setWrongEmail(true)
-      console.log("hello", email, wrongEmail)
-    }
-    if (wrongEmail) {
-      if (password.length > 4 && password.length < 14) {
-        setNoPass(false);
-        console.log(email, password);
-        fetch(`${APIURL}/user/signup`,
-          {
-            method: 'POST',
-            body: JSON.stringify({ user: { email: email, password: password } }),
-            headers: new Headers({ 'Content-Type': 'application/json' })
-          }
-        )
-          .then(
-            (response) => {
-              console.log(response);
-              return response.json()
-            })
-          .then(
-            (data) => {
-              console.log(data);
-              props.updateToken(data.sessionToken)
-            }
-          )
-      } else {
-        setNoPass(true);
-      }
-    } else {
-      (console.log("please correct email format"))
-    }
-  }
-
-
-
+    fetch(`${APIURL}/user/register`, {
+      method: "POST",
+      body: JSON.stringify({ user: { email: email, password: password } }),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        props.updateToken(data.sessionToken);
+      });
+  };
   return (
     <div>
       <h1>Signup</h1>
       <Form onSubmit={handleSubmit}>
         <FormGroup>
-          <Label htmlFor='email'>Email</Label>
-          <Input required onChange={(e) => setEmail(e.target.value)} email='email' value={email} />
-          <p style={{ color: "red" }} > {(wrongEmail) ? "Email is invalid" : ""}</p>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            email="email"
+            value={email}
+          />
         </FormGroup>
         <FormGroup>
-          <Label htmlFor='password'>Password</Label>
-          <Input required onChange={(e) => setPassword(e.target.value)} name='password' value={password} />
-          <p style={{ color: "red" }} > {(noPass) ? "Password must be between 4 and 13 characters" : ""}</p>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            required
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            value={password}
+          />
         </FormGroup>
-        <StyledButton type='submit'>Signup</StyledButton>
+        <StyledButton type="submit">Signup</StyledButton>
       </Form>
     </div>
   );
 };
-
 export default Signup;
-
